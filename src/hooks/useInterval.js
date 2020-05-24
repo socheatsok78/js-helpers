ca4e3b263dc9126f8c9a4b31336ff7254384ace8
Socheat Sok <alex@socheat.net>
2020-05-24 20:08:30 +0700

    fix(core): fix typo

--- src/hooks/useInterval.js
+++ src/hooks/useInterval.js
@@ -3,7 +3,7 @@
  * @param {Number} ms Milliseconds
  * @param {Function} callback
  */
-export default  function useInterval(callback, ms) {
+export default function useInterval(callback, ms) {
     const instance = setInterval(callback, ms);
 
     return () => clearInterval(instance);
