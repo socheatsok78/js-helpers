8f3af44b6d98d267a3f8464c47acccb769d02b4d
Socheat Sok <alex@socheat.net>
2020-05-30 20:24:33 +0700

    build(packaging): build application

--- dist/vue/helpers/W.js
+++ dist/vue/helpers/W.js
@@ -5,22 +5,17 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 const W = {
-  name: 'W',
   props: {
     child: {
       required: true
     }
   },
-
-  template() {
-    return `
-            <component :is="child">
-                <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
-                <template v-for="(_, name) in $scopedSlots" :slot="name" slot-scope="slotData"><slot :name="name" v-bind="slotData" /></template>
-            </component>
-        `;
-  }
-
+  template: `
+        <component :is="child">
+            <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
+            <template v-for="(_, name) in $scopedSlots" :slot="name" slot-scope="slotData"><slot :name="name" v-bind="slotData" /></template>
+        </component>
+    `
 };
 var _default = W;
 exports.default = _default;
