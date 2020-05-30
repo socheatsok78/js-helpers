6ff916217a5e5f34a104ed9b9d247abaf988b93a
Socheat Sok <alex@socheat.net>
2020-05-30 20:24:19 +0700

    fix(core): fix W instance

--- src/vue/helpers/W.js
+++ src/vue/helpers/W.js
@@ -1,18 +1,15 @@
 const W = {
-    name: 'W',
     props: {
         child: {
             required: true
         }
     },
-    template() {
-        return `
-            <component :is="child">
-                <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
-                <template v-for="(_, name) in $scopedSlots" :slot="name" slot-scope="slotData"><slot :name="name" v-bind="slotData" /></template>
-            </component>
-        `
-    }
+    template: `
+        <component :is="child">
+            <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
+            <template v-for="(_, name) in $scopedSlots" :slot="name" slot-scope="slotData"><slot :name="name" v-bind="slotData" /></template>
+        </component>
+    `
 }
 
 export default W
